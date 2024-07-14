4d98ab96072f0f81f46e51fb25b6ed8037f9ba63
Pedro Henrique de Paula Alves <pedrohenriquedepaulaalves@yahoo.com.br>
2024-07-14 16:57:16 -0300

    fix: Fixing host and port

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ import uvicorn
 
 import src.routes.documentation
 import src.routes.upload
-
 from src.routes.app import app
 
-
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=5000)
